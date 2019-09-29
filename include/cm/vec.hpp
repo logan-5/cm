a9f24a1fc4d23a2f5d874164bb253c1b28da3265
logan <logan.r.smith0@gmail.com>
2019-09-28 17:59:37 -0700

    vec accessors

--- include/cm/vec.hpp
+++ include/cm/vec.hpp
@@ -4,6 +4,42 @@
 #include <cm/vec_base.hpp>
 
 namespace cm {
+
+namespace detail {
+#define CM_VEC_ACCESSOR(NAME, IDX)                              \
+    constexpr Rep& NAME() noexcept {                            \
+        return static_cast<VecBase*>(this)->storage[IDX];       \
+    }                                                           \
+    constexpr const Rep& NAME() const noexcept {                \
+        return static_cast<const VecBase*>(this)->storage[IDX]; \
+    }
+template <typename Rep, typename VecBase>
+struct vec_with_access_1D : VecBase {
+    using VecBase::VecBase;
+    CM_VEC_ACCESSOR(x, 0)
+    CM_VEC_ACCESSOR(r, 0)
+};
+template <typename Rep, typename VecBase>
+struct vec_with_access_2D : vec_with_access_1D<Rep, VecBase> {
+    using vec_with_access_1D<Rep, VecBase>::vec_with_access_1D;
+    CM_VEC_ACCESSOR(y, 1)
+    CM_VEC_ACCESSOR(g, 1)
+};
+template <typename Rep, typename VecBase>
+struct vec_with_access_3D : vec_with_access_2D<Rep, VecBase> {
+    using vec_with_access_2D<Rep, VecBase>::vec_with_access_2D;
+    CM_VEC_ACCESSOR(z, 2)
+    CM_VEC_ACCESSOR(b, 2)
+};
+template <typename Rep, typename VecBase>
+struct vec_with_access_4D : vec_with_access_3D<Rep, VecBase> {
+    using vec_with_access_3D<Rep, VecBase>::vec_with_access_3D;
+    CM_VEC_ACCESSOR(w, 3)
+    CM_VEC_ACCESSOR(a, 3)
+};
+#undef CM_VEC_ACCESSOR
+}  // namespace detail
+
 template <typename Rep>
 struct scalar_ : vec_base<Rep, 1> {
    protected:
@@ -22,9 +58,9 @@ using scalar = scalar_<f32>;
 using iscalar = scalar_<i32>;
 
 template <typename Rep>
-struct vec2_ : vec_base<Rep, 2> {
+struct vec2_ : detail::vec_with_access_2D<Rep, vec_base<Rep, 2>> {
    protected:
-    using base = vec_base<Rep, 2>;
+    using base = detail::vec_with_access_2D<Rep, vec_base<Rep, 2>>;
 
    public:
     using base::base;
@@ -36,9 +72,9 @@ using vec2 = vec2_<f32>;
 using ivec2 = vec2_<i32>;
 
 template <typename Rep>
-struct vec3_ : vec_base<Rep, 3> {
+struct vec3_ : detail::vec_with_access_3D<Rep, vec_base<Rep, 3>> {
    protected:
-    using base = vec_base<Rep, 3>;
+    using base = detail::vec_with_access_3D<Rep, vec_base<Rep, 3>>;
 
    public:
     using base::base;
@@ -48,6 +84,20 @@ struct vec3_ : vec_base<Rep, 3> {
 };
 using vec3 = vec3_<f32>;
 using ivec3 = vec3_<i32>;
+
+template <typename Rep>
+struct vec4_ : detail::vec_with_access_4D<Rep, vec_base<Rep, 4>> {
+   protected:
+    using base = detail::vec_with_access_4D<Rep, vec_base<Rep, 4>>;
+
+   public:
+    using base::base;
+
+   protected:
+    using base::storage;
+};
+using vec4 = vec4_<f32>;
+using ivec4 = vec4_<i32>;
 }  // namespace cm
 
 #endif
--- test/vec.cpp
+++ test/vec.cpp
@@ -23,6 +23,31 @@ static_assert(vec3{scalar{5.f}} == vec3{5.f, 5.f, 5.f});
 static_assert(vec2{1.f, 2.f} == vec2{ivec2{1, 2}});
 static_assert(ivec2{1.f, 2.f} == ivec2{vec2{1, 2}});
 
-TEST_CASE("this is a test test") {
-    REQUIRE(0 == 0);
+// access
+static_assert(vec2{1.f, 2.f}.x() == 1.f);
+static_assert(vec2{1.f, 2.f}.y() == 2.f);
+static_assert(vec3{1.f, 2.f, 3.f}.z() == vec3{1.f, 2.f, 3.f}.b());
+
+TEST_CASE("accessors", "[accessors]") {
+    constexpr vec4 v{1.f, 2.f, 3.f, 4.f};
+    static_assert(v.x() == 1.f);
+    static_assert(v.x() == v.r());
+    static_assert(v.y() == 2.f);
+    static_assert(v.y() == v.g());
+    static_assert(v.z() == 3.f);
+    static_assert(v.z() == v.b());
+    static_assert(v.w() == 4.f);
+    static_assert(v.w() == v.a());
+
+    SECTION("writing through one accessor is readable through others") {
+        vec4 vCopy{v};
+        vCopy.x() = 10.f;
+        REQUIRE(vCopy.r() == 10.f);
+        vCopy.y() = 20.f;
+        REQUIRE(vCopy.g() == 20.f);
+        vCopy.z() = 30.f;
+        REQUIRE(vCopy.b() == 30.f);
+        vCopy.w() = 40.f;
+        REQUIRE(vCopy.a() == 40.f);
+    }
 }
